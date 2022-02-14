05fbeaab95408ed5934768385a82ccbfd42953cc
Jānis Alunāns <j.alunans@gmail.com>
2022-02-14 23:09:29 +0200

    Add tests for Rook

---
+++ spec/rook_spec.rb
@@ -0,0 +1,29 @@
+# rook_spec.rb
+require_relative '../lib/rook.rb'
+describe Rook do
+  describe '#possible_moves' do
+    context "a1 square" do
+      subject(:piece) { described_class.new(:white) }
+      let(:placement) { [0, 0] }
+
+      it 'returns correct moves upwards' do
+        correct_moves_up = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]
+        moves_up = piece.possible_moves(placement)[:up]
+        expect(moves_up).to eq(correct_moves_up)
+      end
+
+      it 'returns correct moves rightwards' do
+        correct_rightwards_moves = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]]
+        moves_rightwards = piece.possible_moves(placement)[:right]
+        expect(moves_rightwards).to eq(correct_rightwards_moves)
+      end
+
+      it "doesn't return any superfluous moves" do
+        all_moves = piece.possible_moves(placement)
+        move_types = 2
+        expect(all_moves.size).to eq(move_types)
+      end
+
+    end
+  end
+end
