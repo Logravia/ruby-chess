32212e1de86ff320956cf50f683ce78a2089518e
Jānis Alunāns <j.alunans@gmail.com>
2022-02-14 23:48:34 +0200

    Set-up TODOs and structure for legal_move?

--- lib/arbiter.rb
+++ lib/arbiter.rb
@@ -14,9 +14,25 @@ class Arbiter
     @state = state
   end
 
-  def legal_move?(from, _to)
-    from_col, from_row = from
-    piece = state[from_row][from_col]
+  def legal_move?(from, to)
+    piece = piece_at(from)
+
+    return legal_for_king?(from, to) if piece.class == King
+    return legal_for_pawn?(from, to) if piece.class == Pawn
+
+    # What makes a move legal for most pieces?
+    # TODO: Does not go past a piece
+    # TODO: Does not go on top of a friendly piece
+    # TODO: Follows all the rules set for the pieces movements
+    # TODO: Move does not leave King in check (if after move king in check, illegal)
+  end
+
+  def legal_for_pawn?(from,to)
     false
   end
+
+  def legal_for_king?(from, to)
+    false
+  end
+
 end
