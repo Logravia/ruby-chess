81c1437d5ae3f8fadf77a34d9dc4cdd0f55659f3
Jānis Alunāns <j.alunans@gmail.com>
2022-02-14 15:10:37 +0200

    Implement Pawn

---
+++ lib/pawn.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative 'piece'
+
+class Pawn < Piece
+  CUR_MOVES = ALL_MOVES.select { |move| %i[up down].include? move }
+  attr_accessor :moved
+
+  def initialize(color)
+    super
+    @moved = false
+    @direction = @color == :white ? :up : :down
+  end
+
+  def possible_moves(from)
+    all_moves = super.select { |move| move == @direction }
+    # Pawn can move one square up/down or two depending whether it had moved
+    all_moves[@direction] = @moved ? all_moves[@direction][0] : all_moves[@direction][0..1]
+    all_moves
+  end
+end
