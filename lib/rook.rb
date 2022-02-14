0bbb4ef00d3fdb0779393f8dfdfefd642566c1ac
Jānis Alunāns <j.alunans@gmail.com>
2022-02-14 23:08:52 +0200

    Fix syntax induced bug in Rook

--- lib/rook.rb
+++ lib/rook.rb
@@ -5,11 +5,12 @@ require_relative 'piece'
 class Rook < Piece
   CUR_MOVES = ALL_MOVES.select do |move, _v|
     %i[up down left right].include? move
-    attr_accessor :moved
+  end
 
-    def initialize(color)
-      super
-      @moved = false
-    end
+  attr_accessor :moved
+
+  def initialize(color)
+    super
+    @moved = false
   end
 end
