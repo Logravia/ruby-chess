e652565912c37d12af9ec9f8c2d3cf5cef0a7c05
Jānis Alunāns <j.alunans@gmail.com>
2022-02-15 22:39:59 +0200

    Add check?(king)

--- lib/arbiter.rb
+++ lib/arbiter.rb
@@ -9,23 +9,34 @@ require_relative 'board'
 class Arbiter
   extend RuleHelper
 
-  attr_reader :state
+  attr_reader :state, :kings
 
-  def initialize(state)
-    @state = state
+  def initialize(board)
+    @board = board
+    @state = board.state
+    @kings = board.kings
   end
 
-  def legal_move?(from, to)
-    piece = piece_at(from)
+  def legal_move?(from, to, color)
 
-    return legal_for_king?(from, to) if piece.class == King
-    return legal_for_pawn?(from, to) if piece.class == Pawn
+    @board.temp_move(from, to)
+
+    if check?(@kings[:color])
+      @board.reverse_temp_move
+      return false
+    end
+
+
+    piece = piece_at(from)
+    return legal_for_king?(from, to, piece) if piece.class == King
+    return legal_for_pawn?(from, to, piece) if piece.class == Pawn
 
     # What makes a move legal for most pieces?
     # TODO: Move does not leave King in check (if after move king in check, illegal)
     # TODO: Does not go on top of a friendly piece
     # TODO: Does not go past a piece
     # TODO: Follows all the rules set for the pieces movements
+    @board.reverse_temp_move
   end
 
   def square_under_attack?(by_color, square)
@@ -38,11 +49,16 @@ class Arbiter
     false
   end
 
-  def legal_for_pawn?(from,to)
+  def check?(king)
+    enemy_color = king.color == :white ? :black : :white
+    square_under_attack?(king.color, king.location)
+  end
+
+  def legal_for_pawn?(from,to,pawn)
     false
   end
 
-  def legal_for_king?(from, to)
+  def legal_for_king?(from, to, king)
     false
   end
 
