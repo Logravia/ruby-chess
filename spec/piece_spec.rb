5a0572f006043a3efa329795ff6dc0c47828bda7
Jānis Alunāns <j.alunans@gmail.com>
2022-02-15 11:31:18 +0200

    Update method spec for possible_move

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -5,57 +5,55 @@ require_relative '../lib/piece'
 describe Piece do
   describe '#possible_moves' do
     context 'square a1' do
-      subject(:piece) { described_class.new(:white) }
-      let(:placement) { [0, 0] }
+      subject(:piece) { described_class.new(:white, [0,0]) }
 
       it 'returns correct moves upwards' do
         correct_moves_up = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]
-        moves_up = piece.possible_moves(placement)[:up]
+        moves_up = piece.possible_moves[:up]
         expect(moves_up).to eq(correct_moves_up)
       end
       it 'returns correct moves rightwards' do
         correct_rightwards_moves = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]]
-        moves_rightwards = piece.possible_moves(placement)[:right]
+        moves_rightwards = piece.possible_moves[:right]
         expect(moves_rightwards).to eq(correct_rightwards_moves)
       end
 
       it 'returns correct moves diagonally right-upwards' do
         correct_moves = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]]
-        received_moves = piece.possible_moves(placement)[:diagonal_r_up]
+        received_moves = piece.possible_moves[:diagonal_r_up]
         expect(received_moves).to eq(correct_moves)
       end
 
       it "doesn't return any superfluous moves" do
-        all_moves = piece.possible_moves(placement)
+        all_moves = piece.possible_moves
         move_types = 3
         expect(all_moves.size).to eq(move_types)
       end
     end
 
     context 'square h8' do
-      subject(:piece) { described_class.new(:white) }
-      let(:placement) { [7, 7] }
+      subject(:piece) { described_class.new(:white, [7,7]) }
 
       it 'returns correct moves downwards' do
         correct_moves_down = [[7, 6], [7, 5], [7, 4], [7, 3], [7, 2], [7, 1], [7, 0]]
-        moves_received = piece.possible_moves(placement)[:down]
+        moves_received = piece.possible_moves[:down]
         expect(moves_received).to eq(correct_moves_down)
       end
 
       it 'returns correct moves leftwards' do
         correct_moves_left = [[6, 7], [5, 7], [4, 7], [3, 7], [2, 7], [1, 7], [0, 7]]
-        moves_received = piece.possible_moves(placement)[:left]
+        moves_received = piece.possible_moves[:left]
         expect(moves_received).to eq(correct_moves_left)
       end
 
       it 'returns correct moves diagonally left and down' do
         correct_moves_diagonally = [[6, 6], [5, 5], [4, 4], [3, 3], [2, 2], [1, 1], [0, 0]]
-        moves_received = piece.possible_moves(placement)[:diagonal_l_down]
+        moves_received = piece.possible_moves[:diagonal_l_down]
         expect(moves_received).to eq(correct_moves_diagonally)
       end
 
       it "doesn't return any superfluous moves" do
-        all_moves = piece.possible_moves(placement)
+        all_moves = piece.possible_moves
         move_types = 3
         expect(all_moves.size).to eq(move_types)
       end
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -2,24 +2,23 @@
 require_relative '../lib/rook.rb'
 describe Rook do
   describe '#possible_moves' do
-    context "a1 square" do
-      subject(:piece) { described_class.new(:white) }
-      let(:placement) { [0, 0] }
+    context "rook on a1 square" do
+      subject(:piece) { described_class.new(:white, [0,0]) }
 
       it 'returns correct moves upwards' do
         correct_moves_up = [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7]]
-        moves_up = piece.possible_moves(placement)[:up]
+        moves_up = piece.possible_moves[:up]
         expect(moves_up).to eq(correct_moves_up)
       end
 
       it 'returns correct moves rightwards' do
         correct_rightwards_moves = [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0]]
-        moves_rightwards = piece.possible_moves(placement)[:right]
+        moves_rightwards = piece.possible_moves[:right]
         expect(moves_rightwards).to eq(correct_rightwards_moves)
       end
 
       it "doesn't return any superfluous moves" do
-        all_moves = piece.possible_moves(placement)
+        all_moves = piece.possible_moves
         move_types = 2
         expect(all_moves.size).to eq(move_types)
       end
