3d61b1e2dd8aee3ad4794b2d67f5c5f4b1850025
Jānis Alunāns <j.alunans@gmail.com>
2022-02-17 12:02:42 +0200

    Add Square class

---
+++ square.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+class Square
+  attr_reader :location, :board, :piece
+
+  def initialize(location, board, piece = nil)
+    @location = location
+    @piece = piece
+    @board = board
+  end
+
+  def under_attack?(by_color)
+    pieces = board.pieces_of_color(by_color)
+    pieces.each do |piece|
+      piece.possible_moves.each do |_move_name, move_group|
+        return true if move_group.include? self.location
+      end
+    end
+    false
+  end
+
+  def set_piece(piece_to_set)
+   piece = piece_to_set
+  end
+
+  private
+  attr_writer :piece
+
+end
