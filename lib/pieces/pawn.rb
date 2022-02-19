3b90e04d26828032f7d90ca3b2dfc3502ffe54f7
Jānis Alunāns <j.alunans@gmail.com>
2022-02-19 12:43:23 +0200

    Fix Pawn not returning possible_moves

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -5,19 +5,21 @@ require 'pry-byebug'
 
 class Pawn < Piece
   CUR_MOVES = ALL_MOVES.select { |move| %i[up down].include? move }
-  attr_accessor :moved
+  attr_writer :moved
 
   def initialize(color, square)
     super
     @moved = false
     @direction = @color == :white ? :up : :down
   end
-  # TODO: Write tests for Pawn with particular attention to attacking
+
   def possible_moves
     all_moves = super.select { |move| move == @direction }
     # Pawn can move one square up/down or two depending whether it had moved
-    all_moves[@direction] = @moved ? all_moves[@direction][0] : all_moves[@direction][0..1]
+    all_moves[@direction] = @moved ? [all_moves[@direction][0]] : all_moves[@direction][0..1]
+    #TODO: forbid pawn from jumping on enemy piece or friendly piece for that matter.
     all_moves[:attack_moves] = attack_moves
+    all_moves
   end
 
   def attack_moves
@@ -26,6 +28,7 @@ class Pawn < Piece
     neighboring_pieces.each do |piece|
       attack_moves << piece.location if piece.color != @color
     end
+    attack_moves
   end
 
   def diagonal_neighbors_in_move_direction
