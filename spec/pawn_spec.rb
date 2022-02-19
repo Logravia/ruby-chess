5ba313ba788f5c2093b5812ecfdfa74ea154247f
Jānis Alunāns <j.alunans@gmail.com>
2022-02-19 15:13:23 +0200

    Cover diagonal capture cases in pawn_spec

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -27,7 +27,7 @@ describe Pawn do
       end
 
       it 'can move only one piece upwards after being moved' do
-        pawn.moved = true
+        pawn.available_move_distance = 1
         pawn_moves = pawn.possible_moves[:up]
         correct_moves = [[0,2]]
         expect(pawn_moves).to eq(correct_moves)
@@ -68,7 +68,7 @@ describe Pawn do
       end
 
       it 'can move only one piece downwards after being moved' do
-        pawn.moved = true
+        pawn.available_move_distance = 1
         pawn_moves = pawn.possible_moves[:down]
         correct_moves = [[0,5]]
         expect(pawn_moves).to eq(correct_moves)
@@ -90,7 +90,7 @@ describe Pawn do
 
     end
 
-    context 'white pawn on a2 with a piece above it' do
+    context 'white pawn on a2 with piece above it' do
 
       let(:board){instance_double(Board, square_at: other_squares)}
 
@@ -124,5 +124,70 @@ describe Pawn do
 
     end
 
+    context 'black pawn on a7 with piece below it' do
+
+      let(:board){instance_double(Board, square_at: other_squares)}
+      let(:other_squares){instance_double(Square)}
+
+      let(:my_location){[0,6]}
+      let(:enemy_location){[0,5]}
+
+      let(:my_square){instance_double(Square, location: my_location, board: board)}
+      subject(:my_piece) { described_class.new(:black, my_square) }
+
+      let(:enemy_piece){instance_double(Piece, color: :white, location: enemy_location)}
+
+
+      it 'it does not allow to jump on enemy piece right below it' do
+        allow(other_squares).to receive(:empty?).and_return(false)
+        allow(board).to receive(:piece_at).and_return(enemy_piece)
+
+        correct_moves = []
+        moves_up = my_piece.possible_moves[:down]
+        expect(moves_up).to eq(correct_moves)
+      end
+
+      it 'it does not allow to jump on enemy piece two squares below it' do
+        allow(other_squares).to receive(:empty?).and_return(true, true, false, true)
+        allow(board).to receive(:piece_at).and_return(enemy_piece)
+
+        correct_moves = [[0,5]]
+        moves_up = my_piece.possible_moves[:down]
+        expect(moves_up).to eq(correct_moves)
+      end
+
+    end
+
+    context 'white pawn on a2 with piece above and to the right of it' do
+
+      let(:board){instance_double(Board, square_at: other_squares)}
+      let(:other_squares){instance_double(Square)}
+
+      let(:other_pieces_location){[1,2]}
+
+      let(:my_location){[0,1]}
+      let(:my_square){instance_double(Square, location: my_location, board: board)}
+      subject(:my_piece) { described_class.new(:white, my_square) }
+
+      let(:other_piece){instance_double(Piece, location: other_pieces_location)}
+
+      before do
+        allow(other_squares).to receive(:empty?).and_return(false)
+        allow(board).to receive(:piece_at).and_return(other_piece,nil)
+      end
+
+      it "include enemy pieces location in its attack_moves" do
+        allow(other_piece).to receive(:color).and_return(:black)
+        attack_moves = my_piece.possible_moves[:attack_moves]
+        expect(attack_moves).to eq([other_pieces_location])
+      end
+
+      it 'it does not include friendly piece in its attack_moves' do
+        allow(other_piece).to receive(:color).and_return(:white)
+        attack_moves = my_piece.possible_moves[:attack_moves]
+        expect(attack_moves).to be_empty
+      end
+
+    end
   end
 end
