13c4767c35865ca44e88a124390a509850ed5413
Jānis Alunāns <j.alunans@gmail.com>
2022-02-21 17:34:35 +0200

    Remove make_fen(board) from FenParse

--- lib/fen_parser.rb
+++ lib/fen_parser.rb
@@ -15,6 +15,7 @@ module FenParser
 
   private_constant :ROW_SEPARATOR, :PIECE_LETTERS
 
+  # TODO: Write tests for make_board
   def make_board(fen)
     board = []
     fen.split(ROW_SEPARATOR).each_with_index do |fen_row, row_num|
@@ -54,24 +55,5 @@ module FenParser
   def build_piece(piece_char, square)
     color = piece_color(piece_char)
     PIECE_LETTERS[piece_char.downcase.to_sym].new(color, square)
-  end
-
-  ## TODO: modularize and make work
-  def make_fen(board)
-    fen_string = ""
-    board.each do |row|
-      nil_count = 0
-      row.each do |piece|
-        if piece.nil?
-         nil_count += 1
-        else
-          fen_string << nil_count.to_s if nil_count > 0
-          nil_count = 0
-          fen_string << PIECE_LETTERS.key(piece.class).to_s
-        end
-      end
-      fen_string << ROW_SEPARATOR
-    end
-    fen_string.chomp(ROW_SEPARATOR)
   end
 end
