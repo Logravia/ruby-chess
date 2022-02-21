aba26e5a72aae266a0e8249c1d501f37c1ad457c
Jānis Alunāns <j.alunans@gmail.com>
2022-02-21 16:25:36 +0200

    Add castling_line to King

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -9,12 +9,19 @@ class King < Piece
     @enemy_color = @color == :white ? :black : :white
   end
 
+  # King moves just like a Queen except only one square in all directions
   def possible_moves
     all_moves = super
     all_moves.each_pair do |move_name, squares|
-      # King moves just like a Queen except only one square in all directions
       all_moves[move_name] = squares.first
     end
+  end
+
+  def castling_line(direction)
+    return nil if moved
+    line = []
+    2.times {line << sum_coordinates(location, ALL_MOVES[direction])}
+    line
   end
 
   def checked?
