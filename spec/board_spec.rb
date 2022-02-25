91711def3d83f704665c17b403b85a04e5cf6667
Jānis Alunāns <j.alunans@gmail.com>
2022-02-25 23:14:48 +0200

    Add tests to board_spec for en passant handling

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -46,4 +46,71 @@ describe Board do
       end
     end
 
+    context 'en passant' do
+
+      let(:fen_string) { 'p7/8/1P6/8/8/8/8/8/' }
+      subject(:board) { described_class.new(fen_string) }
+
+      it 'sets en_passant square after pawns double move' do
+        black_pawn = [0,0]
+        two_squares_down = [0,2]
+        en_passant_square = [0,1]
+        board.move(black_pawn, two_squares_down)
+        expect(board.square_at(en_passant_square)).to be_en_passant_square
+      end
+
+      it 'does not not set en_passant square after single pawn move' do
+        black_pawn = [0,0]
+        one_square_down = [0,1]
+        en_passant_square = [0,0]
+        board.move(black_pawn, one_square_down)
+        expect(board.square_at(en_passant_square)).not_to be_en_passant_square
+      end
+
+      it 'removes en_passant square if it has not been taken advantage of' do
+        black_pawn = [0,0]
+        white_pawn = [1,2]
+        two_squares_down = [0,2]
+        one_square_up = [1,1]
+        en_passant_location = [0,1]
+
+        board.move(black_pawn, two_squares_down)
+        board.move(white_pawn, one_square_up)
+        board.move([0,2], [0,3])
+        en_passant_status = board.square_at(en_passant_location).en_passant_square?
+        expect(en_passant_status).to be false
+      end
+
+      it 'removes piece after pawn moves on en_passant square' do
+        black_pawn = [0,0]
+        white_pawn = [1,2]
+        two_squares_down = [0,2]
+        en_passant_location = [0,1]
+
+        board.move(black_pawn, two_squares_down)
+        board.move(white_pawn, en_passant_location)
+        expect(board.square_at(two_squares_down)).to be_empty
+
+      end
+
+      context 'Bishop attempting to do en_passant' do
+        let(:fen_string) { 'p7/8/1B6/8/8/8/8/8/' }
+        subject(:board) { described_class.new(fen_string) }
+
+        it 'does not remove piece when a piece other than enemy pawn moves on it' do
+        black_pawn = [0,0]
+        white_bishop = [1,2]
+        two_squares_down = [0,2]
+        en_passant_location = [0,1]
+
+        board.move(black_pawn, two_squares_down)
+        board.move(white_bishop, en_passant_location)
+        expect(board.square_at(two_squares_down)).not_to be_empty
+        Display.new.show_board(board.state)
+
+        end
+      end
+
+    end
   end
+end
