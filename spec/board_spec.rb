27f98a9ee942adcb2d2d6790457654b23423fd91
Jānis Alunāns <j.alunans@gmail.com>
2022-02-25 12:38:53 +0200

    Add castling tests to board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,33 +4,46 @@
 
 require_relative '../lib/board'
 require_relative '../lib/square'
+require_relative '../lib/display'
 
 # TODO: Add tests
 
 describe Board do
-  let(:fen_string) { 'R6r/8/8/8/8/8/8/8/' }
+  let(:fen_string) { 'R3K2R/8/8/8/8/8/8/8/' }
   subject(:board) { described_class.new(fen_string) }
 
-  describe '#move_piece' do
+  describe '#move' do
     it 'moves piece from [0,0] to [1,0]' do
       from = [0, 0]
       to = [1, 0]
       piece_to_move = board.piece_at(from)
-      board.move_piece(from, to)
+      board.move(from, to)
       expect(board.piece_at(to)).to be(piece_to_move)
     end
 
-    it 'moves piece to a square when there is another pieces there' do
+    it 'performs castling rightwarwds' do
+      kings_location = [4,0]
+      right_castle_square = [6,0]
+      where_rook_should_be = [5,0]
+
+      board.move(kings_location, right_castle_square)
+
+      expect(board.piece_at(right_castle_square).class).to be(King)
+      expect(board.piece_at(where_rook_should_be).class).to be(Rook)
+    end
+
+    it 'performs castling leftwards' do
+      kings_location = [4,0]
+      right_castle_square = [2,0]
+      where_rook_should_be = [3,0]
+
+      Display.new.show_board(board.state)
+      board.move(kings_location, right_castle_square)
+      Display.new.show_board(board.state)
+
+      expect(board.piece_at(right_castle_square).class).to be(King)
+      expect(board.piece_at(where_rook_should_be).class).to be(Rook)
     end
   end
 
-  describe '#note_move' do
-    it 'saves state of the board before the move' do
-    end
   end
-
-  describe '#undo_move' do
-    it 'undos the move' do
-    end
-  end
-end
