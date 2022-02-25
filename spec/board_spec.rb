c4ff74a0be71ebbd789a5960e3eaa301f157f1b8
Jānis Alunāns <j.alunans@gmail.com>
2022-02-25 18:25:59 +0200

    Add test cases for castling in board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,41 +9,41 @@ require_relative '../lib/display'
 # TODO: Add tests
 
 describe Board do
-  let(:fen_string) { 'R3K2R/8/8/8/8/8/8/8/' }
-  subject(:board) { described_class.new(fen_string) }
 
   describe '#move' do
-    it 'moves piece from [0,0] to [1,0]' do
-      from = [0, 0]
-      to = [1, 0]
-      piece_to_move = board.piece_at(from)
-      board.move(from, to)
-      expect(board.piece_at(to)).to be(piece_to_move)
+
+    context 'castling' do
+      let(:fen_string) { 'R3K2R/8/8/8/8/8/8/8/' }
+      subject(:board) { described_class.new(fen_string) }
+      it 'moves piece from [0,0] to [1,0]' do
+        from = [0, 0]
+        to = [1, 0]
+        piece_to_move = board.piece_at(from)
+        board.move(from, to)
+        expect(board.piece_at(to)).to be(piece_to_move)
+      end
+
+      it 'performs castling rightwards' do
+        kings_location = [4,0]
+        right_castle_square = [6,0]
+        where_rook_should_be = [5,0]
+
+        board.move(kings_location, right_castle_square)
+
+        expect(board.piece_at(right_castle_square).class).to be(King)
+        expect(board.piece_at(where_rook_should_be).class).to be(Rook)
+      end
+
+      it 'performs castling leftwards' do
+        kings_location = [4,0]
+        right_castle_square = [2,0]
+        where_rook_should_be = [3,0]
+
+        board.move(kings_location, right_castle_square)
+
+        expect(board.piece_at(right_castle_square).class).to be(King)
+        expect(board.piece_at(where_rook_should_be).class).to be(Rook)
+      end
     end
-
-    it 'performs castling rightwarwds' do
-      kings_location = [4,0]
-      right_castle_square = [6,0]
-      where_rook_should_be = [5,0]
-
-      board.move(kings_location, right_castle_square)
-
-      expect(board.piece_at(right_castle_square).class).to be(King)
-      expect(board.piece_at(where_rook_should_be).class).to be(Rook)
-    end
-
-    it 'performs castling leftwards' do
-      kings_location = [4,0]
-      right_castle_square = [2,0]
-      where_rook_should_be = [3,0]
-
-      Display.new.show_board(board.state)
-      board.move(kings_location, right_castle_square)
-      Display.new.show_board(board.state)
-
-      expect(board.piece_at(right_castle_square).class).to be(King)
-      expect(board.piece_at(where_rook_should_be).class).to be(Rook)
-    end
-  end
 
   end
