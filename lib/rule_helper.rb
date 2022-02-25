115cd1a6973e85836d37b087064fa397fca8bba8
Jānis Alunāns <j.alunans@gmail.com>
2022-02-25 18:17:32 +0200

    Remove unneeded constants from RuleHelper

--- lib/rule_helper.rb
+++ lib/rule_helper.rb
@@ -9,14 +9,10 @@ module RuleHelper
 
   DEFAULT_BOARD = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
 
-  CASTLING_SQUARES = { white: { [2, 0] => :left, [6, 0] => :right },
-                       black: { [2, 7] => :left, [6, 7] => :right } }.freeze
-
   def self.within_board?(coordinates)
     coordinates[0].between?(X[:min], X[:max]) and coordinates[1].between?(Y[:min], Y[:max])
   end
 
-  def self.move_type(from, to, board)
   def self.move_type(from, destination, board)
     piece = board.piece_at(from)
 
@@ -30,8 +26,5 @@ module RuleHelper
       :normal_move
     end
 
-    # TODO: implement en_passant move type check
-    # return :en_passant if piece.is_a?(Pawn) and
-    :normal_move
   end
 end
