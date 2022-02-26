c428452ef7e9beb36a04beabf16de6633702008a
Jānis Alunāns <j.alunans@gmail.com>
2022-02-26 18:10:29 +0200

    Update square_spec mocks

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -18,20 +18,20 @@ describe Square do
     end
 
     it 'returns true when rook can land on square' do
-      rooks_moves = { down: [[0, 0]] }
-      allow(rook).to receive(:categorized_possible_moves).and_return(rooks_moves)
+      rooks_moves = [[0, 0]]
+      allow(rook).to receive(:moves).and_return(rooks_moves)
       expect(square_under_attack).to be_under_attack(:w)
     end
 
     it 'returns false when rook cannot land on square' do
-      rooks_moves = { right: [[1, 1]] }
-      allow(rook).to receive(:categorized_possible_moves).and_return(rooks_moves)
+      rooks_moves = [[1, 1]]
+      allow(rook).to receive(:moves).and_return(rooks_moves)
       expect(square_under_attack).not_to be_under_attack(:w)
     end
 
     it 'returns false when rook cannot land on square' do
-      rooks_moves = { right: [[0, 1], [1, 0]] }
-      allow(rook).to receive(:categorized_possible_moves).and_return(rooks_moves)
+      rooks_moves = [[0, 1], [1, 0]]
+      allow(rook).to receive(:moves).and_return(rooks_moves)
       expect(square_under_attack).not_to be_under_attack(:w)
     end
   end
