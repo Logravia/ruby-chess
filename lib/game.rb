70777c27bc0a12adbb8fa74be6ba862c0a339ccf
Jānis Alunāns <j.alunans@gmail.com>
2022-02-27 14:34:14 +0200

    Add update_screen

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,12 @@ require_relative 'arbiter'
 require_relative 'input'
 
 class Game
+
+  def update_screen
+    display.clear_screen
+    display.show_board(board.state)
+  end
+
   def focus_piece(piece)
     display.focused_piece = board.piece_at(piece)
   end
