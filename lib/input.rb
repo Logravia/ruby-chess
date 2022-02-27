7baffcc391a4f0a6977317178ebfc426f9724438
Jānis Alunāns <j.alunans@gmail.com>
2022-02-27 14:33:05 +0200

    Fix bugs in Input

--- lib/input.rb
+++ lib/input.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# Asks player for input in algabraic notion ('a2') and returns it in numeric [0,1]
+# Asks player for input in algabraic notion ('a2') and returns it in numeric [7,1]
 # In case of special input, e.g. q or h or s, lets the game handle that.
 class Input
   LEGIT_COORDINATE_INPUT = /^[A-Za-z][0-9]$/.freeze
-  SPECIAL_INPUT = /[qhs]/.freeze
+  SPECIAL_INPUT = /^[qhs]$/.freeze
 
   def initialize(game)
     @game = game
   end
 
-  def square_choice
+  def choice
     to_numeric_coordinates(algrabeic_coordinates)
   end
 
@@ -20,7 +20,6 @@ class Input
   def algrabeic_coordinates
     loop do
       input = gets.chomp
-
       special_input_sender(input)
       return input if input.match?(LEGIT_COORDINATE_INPUT)
 
@@ -30,15 +29,17 @@ class Input
 
   def special_input_sender(input)
     if input.match?(SPECIAL_INPUT)
-      game.handle_special_input(input)
+      @game.handle_special_input(input)
       return
     end
   end
 
   def to_numeric_coordinates(algrabeic_coords)
-    letter, str_number = algrabeic_coords
+    letter, str_number = algrabeic_coords.split('')
     x_coord = letter.upcase.codepoints.pop % 65
-    y_coord = str_number.to_i - 1
+    # y_coord must be flipped as a1 must mean [0,7]
+    # The white pieces are at the bottom of the array, not the start.
+    y_coord = (str_number.to_i - 8).abs
     [x_coord, y_coord]
   end
 
