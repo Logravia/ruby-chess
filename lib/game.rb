85b659e4616ae76974474118e9619465a4ef523c
Jānis Alunāns <j.alunans@gmail.com>
2022-02-27 14:34:43 +0200

    Add choose_destination

--- lib/game.rb
+++ lib/game.rb
@@ -7,6 +7,14 @@ require_relative 'input'
 
 class Game
 
+  def choose_destination_for(chosen_piece)
+    loop do
+      destination = input.choice
+      return destination if arbiter.legal_move?(chosen_piece, destination)
+      puts "Sorry, that is an illegal move."
+    end
+  end
+
   def handle_special_input(input)
     case input
     when 'q'
