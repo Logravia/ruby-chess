948093f34ad1768a19d34aa7dbb4ce1347ffcff3
Jānis Alunāns <j.alunans@gmail.com>
2022-03-02 16:33:08 +0200

    Implement save in SaveLoad

--- lib/save_load.rb
+++ lib/save_load.rb
@@ -3,7 +3,11 @@
 # lib/save_load.rb
 
 module SaveLoad
-  def self.save; end
+  def save
+    file_name = Time.now.strftime("saves/save_%d-%m-%Y-%H-%M-%S")
+    data = Marshal.dump({players: @players, board: @board, turn: @turn})
+    File.write(file_name, data)
+  end
 
   def self.load; end
 end
