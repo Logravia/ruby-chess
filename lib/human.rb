7fa9b7248760cf0ecc4eab962cf0becef452813b
Jānis Alunāns <j.alunans@gmail.com>
2022-03-02 18:20:36 +0200

    Add Human class

---
+++ lib/human.rb
@@ -0,0 +1,18 @@
+# lib/human.rb
+
+require_relative 'input'
+require_relative 'player'
+
+class Human < Player
+  attr_reader :color
+
+  def initialize(color, game)
+    @color = color
+    @game = game
+    @input = Input.new(game)
+  end
+
+  def choice
+    @input.choice
+  end
+end
